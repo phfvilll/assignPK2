2f836f969ec74537c82470abc455452e863c5f66
phfil <81833397+phfil@users.noreply.github.com>
2021-06-27 18:28:19 +0200

    set up the project folder

---
+++ contram/src/main/java/com/acme/contram/ContramApplication.java
@@ -0,0 +1,13 @@
+package com.acme.contram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContramApplication.class, args);
+	}
+
+}
---
+++ contram/src/test/java/com/acme/contram/ContramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.contram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
